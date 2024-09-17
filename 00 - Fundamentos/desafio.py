1e41b38cc04b09ec4707a06e4b6b7e23bf026166
Clézio <cleziojr9@gmail.com>
2024-09-17 16:11:30 -0300

    :sparkles: feat: adição do limite diário de saques

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -7,6 +7,7 @@ menu = """
 
 """
 def operacoes(valor_inicial):
+    QUANTIDADE_DE_SAQUES = 0
     while True:
         print("\nOlá! Seja bem-vindo ao Banco Virtual\nAbaixo, você pode visualizar o menu de serviços oferecidos.\n")
         print(menu)
@@ -19,14 +20,20 @@ def operacoes(valor_inicial):
                 valor_inicial += valor_depositado
                 print("\nOperação efetuada com sucesso.\n")
         elif opcao == 's':
-            valor_de_saque = float(input("\nInforme a seguir o valor, em reais, que você deseja sacar: "))
-            if valor_de_saque <= 0.00:
-                print("\nValor inválido! \n Operação cancelada. \n")
-            elif valor_de_saque > valor_inicial:
-                print("\nO valor de saque solicitado é superior ao saldo atual!\n Operação cancelada. \n")
+            if QUANTIDADE_DE_SAQUES > 3:
+                print("\nO limite diário de operações de saque foi atingido.\nOperação cancelada.\n")
             else:
-                valor_inicial -= valor_de_saque
-                print("\nOperação efetuada com sucesso.\n")
+                valor_de_saque = float(input("\nInforme a seguir o valor, em reais, que você deseja sacar: "))
+                if valor_de_saque <= 0.00:
+                    print("\nValor inválido! \n Operação cancelada. \n")
+                elif (valor_de_saque > valor_inicial):
+                    print("\nO valor de saque solicitado é superior ao saldo atual!\n Operação cancelada. \n")
+                elif valor_de_saque > 500.00:
+                    print("\nO valor de saque solicitado é superior ao limite permitido para cada operação de saque!\n Operação cancelada. \n")
+                else:
+                    valor_inicial -= valor_de_saque
+                    QUANTIDADE_DE_SAQUES += 1
+                    print("\nOperação efetuada com sucesso.\n")
         elif opcao == 'e':
             print(f"\n\nO saldo atual corresponde à R${valor_inicial:.2f}\n\n")
         elif opcao == 'q':
